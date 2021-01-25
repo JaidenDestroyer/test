ccccc235277486b36e2a4658f26f69804e9371aa
JaidenDestroyer <38144027+JaidenDestroyer@users.noreply.github.com>
2021-01-24 21:54:37 -0500

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,6 +1,12 @@
 @rem TEMP CODE
 @echo off
 
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ
-
+start https://www.youtube.com/watch?v=G1IbRujko-A
+timeout 5
+start https://www.youtube.com/watch?v=G1IbRujko-A
+timeout 5
+start https://www.youtube.com/watch?v=G1IbRujko-A
+timeout 5
+start https://www.youtube.com/watch?v=G1IbRujko-A
+timeout 5
 exit
