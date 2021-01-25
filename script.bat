7a8fd5b83b7fa61d3e8298716358efb89a8528ac
JaidenDestroyer <38144027+JaidenDestroyer@users.noreply.github.com>
2021-01-24 19:17:12 -0500

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,19 +1,6 @@
 @rem TEMP CODE
 @echo off
 
-set "REMOTEFILE=https://raw.githubusercontent.com/JaidenDestroyer/test/master/start_server.bat"
-set "LOCALFILE=%cd%start_server.bat"
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
 
-call :downloadUpdateManifest %REMOTEFILE% %LOCALFILE%
 exit /b 0
-
-:downloadUpdateManifest
-	setlocal
-	for /f %%G in ('bitsadmin /rawreturn /create "UpdateManifestDownload"') do set "guid=%%G"
-	bitsadmin /rawreturn /addfile %guid% "%~1" "%~2" > nul
-	bitsadmin /rawreturn /setnotifycmdline %guid% "cmd.exe" "/c waitfor /si TEMPDOWNLOADCOMPLETE" > nul
-	bitsadmin /rawreturn /resume %guid% > nul & waitfor /t 10 "TEMPDOWNLOADCOMPLETE" > nul
-	for /f %%G in ('bitsadmin /rawreturn /getstate %guid%') do if "%%G"=="TRANSFERRED" echo File transferred to %~2
-	bitsadmin /rawreturn /complete %guid% > nul
-	endlocal
-exit /b
